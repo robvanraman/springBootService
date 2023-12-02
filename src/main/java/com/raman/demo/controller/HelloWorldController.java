afa6c8dd23763bf44731dd6bea6fc63f482b28eb
robvanraman <54682798+robvanraman@users.noreply.github.com>
2023-12-02 12:35:20 +0530

    Add files via upload

---
+++ src/main/java/com/raman/demo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.raman.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public  String sayHello() {
+        return "Hello";
+    }
+
+    @GetMapping("/{id}")
+    public  String sayHelloName(@PathVariable String id) {
+        return "Hello "  + id;
+    }
+}
